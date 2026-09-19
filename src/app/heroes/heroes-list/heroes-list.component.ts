ff15750b1ae228251e66e5ec4491673e749680bd
sara <86625@alumnos.utleon.edu.mx>
2026-09-18 20:54:09 -0600

    ngModel

--- src/app/heroes/heroes-list/heroes-list.component.ts
+++ src/app/heroes/heroes-list/heroes-list.component.ts
@@ -7,5 +7,44 @@ import { Component } from '@angular/core';
   styleUrl: './heroes-list.component.css'
 })
 export class HeroesListComponent {
+  imageWidth:number=40;
+  imageMargin:number=2;
+  muestraImage:boolean=true;
+  listFilter:string='';
+
+  showImage():void{
+    this.muestraImage=!this.muestraImage
+  }
+
+  heroes:any[]=[
+    {
+      imagen:"https://dragonball-api.com/characters/goku_normal.webp",
+      nombre:"Goku",
+      descripcion:"Kame hame Ha",
+      race:"Saiyan",
+      Ki:9000
+    },
+     {
+      imagen:"https://dragonball-api.com/characters/vegeta_normal.webp",
+      nombre:"Vegeta",
+      descripcion:"Final Flash",
+      race:"Saiyan",
+      Ki:8500
+    },
+     {
+      imagen:"https://dragonball-api.com/characters/picolo_normal.webp",
+      nombre:"Picolo",
+      descripcion:"Picololololo",
+      race:"Saiyan",
+      Ki:7200
+    },
+     {
+      imagen:"https://dragonball-api.com/characters/bulma.webp",
+      nombre:"Bulma",
+      descripcion:"Bulmaa es el mejor",
+      race:"Saiyan",
+      Ki:2500
+    },
+  ]
 
 }
